75c751b4d1d3dade1b646732d28b3ba01cf32dbc
Julia <222596@edu.p.lodz.pl>
2021-01-12 19:27:10 +0100

    wygenerowanie pola getter

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,6 +9,10 @@ public class DefaultCountingOutRhymer {
 
 	private int total = TOTAL;
 
+	public int getTotal() {
+		return total;
+	}
+
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
